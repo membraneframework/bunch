02b1ba380013338731e49c1ba36e9b082b761e08
Mateusz Front <mateusz.front@swmansion.com>
2018-08-30 13:35:54 +0200

    Improved docs for Bunch.Struct and Bunch.Access modules

--- lib/bunch/access.ex
+++ lib/bunch/access.ex
@@ -2,30 +2,47 @@ defmodule Bunch.Access do
   @moduledoc """
   A bunch of functions for easier manipulation on terms of types implementing `Access`
   behaviour.
-
-  Behaviour differs from Kernel implementations in some aspects:
-  - empty lists of keys are allowed
-  - single key does not have to be wrapped in a list
-  - `delete_in/2` function is provided
   """
   import Kernel, except: [get_in: 2, put_in: 2, update_in: 3, get_and_update_in: 3, pop_in: 2]
   use Bunch
 
   @compile {:inline, map_keys: 1}
 
+  @gen_common_docs fn fun_name ->
+    """
+    Works like `Kernel.#{fun_name}` with small differences.
+
+    Behaviour differs in the following aspects:
+    - empty lists of keys are allowed
+    - single key does not have to be wrapped in a list
+    """
+  end
+
+  @doc """
+  #{@gen_common_docs.("get_in/2")}
+  """
   @spec get_in(Access.t(), Access.key() | [Access.key()]) :: Access.value()
   def get_in(container, []), do: container
   def get_in(container, keys), do: container |> Kernel.get_in(keys |> map_keys)
 
+  @doc """
+  #{@gen_common_docs.("put_in/3")}
+  """
   @spec put_in(Access.t(), Access.key() | [Access.key()], Access.value()) :: Access.value()
   def put_in(_map, [], v), do: v
   def put_in(container, keys, v), do: container |> Kernel.put_in(keys |> map_keys, v)
 
+  @doc """
+  #{@gen_common_docs.("update_in/3")}
+  """
   @spec update_in(Access.t(), Access.key() | [Access.key()], (Access.value() -> Access.value())) ::
           Access.t()
   def update_in(container, [], f), do: f.(container)
   def update_in(container, keys, f), do: container |> Kernel.update_in(keys |> map_keys, f)
 
+  @doc """
+  #{@gen_common_docs.("get_and_update_in/3")}
+  """
   @spec get_and_update_in(Access.t(), Access.key() | [Access.key()], (a -> a)) :: {a, Access.t()}
         when a: Access.value()
   def get_and_update_in(container, [], f), do: f.(container)
@@ -33,10 +50,16 @@ defmodule Bunch.Access do
   def get_and_update_in(container, keys, f),
     do: container |> Kernel.get_and_update_in(keys |> map_keys, f)
 
+  @doc """
+  #{@gen_common_docs.("pop_in/2")}
+  """
   @spec pop_in(Access.t(), Access.key() | [Access.key()]) :: {Access.value(), Access.t()}
   def pop_in(container, []), do: {nil, container}
   def pop_in(container, keys), do: container |> Kernel.pop_in(keys |> map_keys)
 
+  @doc """
+  Works like `pop_in/2`, but discards returned value.
+  """
   @spec delete_in(Access.t(), Access.key() | [Access.key()]) :: Access.t()
   def delete_in(container, keys), do: pop_in(container, keys) ~> ({_out, container} -> container)
 
--- lib/bunch/struct.ex
+++ lib/bunch/struct.ex
@@ -1,33 +1,55 @@
 defmodule Bunch.Struct do
   @moduledoc """
-  A bunch of functions for easier manipulation on structs that do not implement
-  `Access` behaviour.
-
-  For more details see `Bunch.Access` module.
+  A bunch of functions for easier manipulation on structs.
   """
   import Kernel, except: [get_in: 2, put_in: 2, update_in: 3, get_and_update_in: 3, pop_in: 2]
   use Bunch
 
   @compile {:inline, map_keys: 1}
 
+  @gen_common_docs fn fun_name ->
+    """
+    Wraps `Bunch.Access.#{fun_name}` to make it work with structs that do not
+    implement `Access` behaviour.
+    """
+  end
+
+  @doc """
+  #{@gen_common_docs.("get_in/2")}
+  """
   @spec get_in(struct, Access.key() | [Access.key()]) :: Access.value()
   def get_in(struct, keys), do: struct |> Bunch.Access.get_in(keys |> map_keys())
 
+  @doc """
+  #{@gen_common_docs.("put_in/3")}
+  """
   @spec put_in(struct, Access.key() | [Access.key()], Access.value()) :: Access.value()
   def put_in(struct, keys, v), do: struct |> Bunch.Access.put_in(keys |> map_keys(), v)
 
+  @doc """
+  #{@gen_common_docs.("update_in/3")}
+  """
   @spec update_in(struct, Access.key() | [Access.key()], (Access.value() -> Access.value())) ::
           struct
   def update_in(struct, keys, f), do: struct |> Bunch.Access.update_in(keys |> map_keys(), f)
 
+  @doc """
+  #{@gen_common_docs.("get_and_update_in/3")}
+  """
   @spec get_and_update_in(struct, Access.key() | [Access.key()], (a -> a)) :: {a, struct}
         when a: Access.value()
   def get_and_update_in(struct, keys, f),
     do: struct |> Bunch.Access.get_and_update_in(keys |> map_keys(), f)
 
+  @doc """
+  #{@gen_common_docs.("pop_in/2")}
+  """
   @spec pop_in(struct, Access.key() | [Access.key()]) :: {Access.value(), struct}
   def pop_in(struct, keys), do: struct |> Bunch.Access.pop_in(keys |> map_keys())
 
+  @doc """
+  #{@gen_common_docs.("delete_in/2")}
+  """
   @spec delete_in(struct, Access.key() | [Access.key()]) :: struct
   def delete_in(struct, keys), do: struct |> Bunch.Access.delete_in(keys |> map_keys())
 
