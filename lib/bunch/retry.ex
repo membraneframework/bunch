b4cdb61816339f6ec529ae92772aefea3db158dd
Mateusz Front <mateusz.front@swmansion.com>
2018-08-30 12:19:26 +0200

    refactor of Retry module

--- lib/bunch/retry.ex
+++ lib/bunch/retry.ex
@@ -4,47 +4,72 @@ defmodule Bunch.Retry do
   until it succeeds.
   """
 
-  @type retry_option ::
+  @typedoc """
+  Possible options for `retry/3`.
+  """
+  @type retry_option_t ::
           {:times, non_neg_integer()}
           | {:duration, milliseconds :: pos_integer}
           | {:delay, milliseconds :: pos_integer}
 
   @doc """
   Calls `fun` function until `arbiter` function decides to stop.
+
+  Possible options are:
+  - times - limits amount of retries (first evaluation is not considered a retry)
+  - duration - limits total time of execution of this function, but breaks only
+  before subsequent retry
+  - delay - introduces delay (`:timer.sleep/1`) before each retry
+
+  ## Examples
+  ```
+  iex> {:ok, pid} = Agent.start_link(fn -> 0 end)
+  iex> #{__MODULE__}.retry(fn -> Agent.get_and_update(pid, &{&1, &1+1}) end, & &1 > 3)
+  4
+  iex> {:ok, pid} = Agent.start_link(fn -> 0 end)
+  iex> #{__MODULE__}.retry(fn -> Agent.get_and_update(pid, &{&1, &1+1}) end, & &1 > 3, times: 10)
+  4
+  iex> {:ok, pid} = Agent.start_link(fn -> 0 end)
+  iex> #{__MODULE__}.retry(fn -> Agent.get_and_update(pid, &{&1, &1+1}) end, & &1 > 3, times: 2)
+  2
+  iex> {:ok, pid} = Agent.start_link(fn -> 0 end)
+  iex> #{__MODULE__}.retry(
+  ...> fn -> :timer.sleep(10); Agent.get_and_update(pid, &{&1, &1+1}) end,
+  ...> & &1 > 3,
+  ...> duration: 25
+  ...> )
+  2
+  iex> {:ok, pid} = Agent.start_link(fn -> 0 end)
+  iex> #{__MODULE__}.retry(
+  ...> fn -> :timer.sleep(7); Agent.get_and_update(pid, &{&1, &1+1}) end,
+  ...> & &1 > 3,
+  ...> duration: 25, delay: 3
+  ...> )
+  2
+  ```
   """
   @spec retry(
           fun :: (() -> res),
-          arbiter :: (res -> :retry | :finish),
-          params :: [retry_option()]
+          arbiter :: (res -> stop? :: boolean),
+          options :: [retry_option_t()]
         ) :: res
         when res: any()
-  def retry(fun, arbiter, params) do
-    times = params |> Keyword.get(:times, :infinity)
-    duration = params |> Keyword.get(:duration, :infinity)
-    delay = params |> Keyword.get(:delay, 0)
+  def retry(fun, arbiter, options \\ []) do
+    times = options |> Keyword.get(:times, :infinity)
+    duration = options |> Keyword.get(:duration, :infinity)
+    delay = options |> Keyword.get(:delay, 0)
     fun |> do_retry(arbiter, times, duration, delay, 0, System.monotonic_time(:milliseconds))
   end
 
   defp do_retry(fun, arbiter, times, duration, delay, retries, init_time) do
     ret = fun.()
 
-    case arbiter.(ret) do
-      :finish ->
-        ret
-
-      :retry ->
-        cond do
-          times |> infOrGt(retries) &&
-              duration |> infOrGt(System.monotonic_time(:milliseconds) - init_time + delay) ->
-            :timer.sleep(delay)
-            fun |> do_retry(arbiter, times, duration, delay, retries + 1, init_time)
-
-          true ->
-            ret
-        end
+    if not arbiter.(ret) and times > retries &&
+         duration > System.monotonic_time(:milliseconds) - init_time + delay do
+      :timer.sleep(delay)
+      fun |> do_retry(arbiter, times, duration, delay, retries + 1, init_time)
+    else
+      ret
     end
   end
-
-  defp infOrGt(:infinity, _), do: true
-  defp infOrGt(val, other), do: val > other
 end
