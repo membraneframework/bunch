733e7220cd101aefbc880d9567e9afe43cd6212a
Mateusz Front <mateusz.front@swmansion.com>
2018-09-12 17:02:25 +0200

    added Bunch.Map and Bunch.TupleList modules

---
+++ lib/bunch/map.ex
@@ -0,0 +1,33 @@
+defmodule Bunch.Map do
+  @moduledoc """
+  A bunch of helper functions for manipulating maps.
+  """
+
+  @doc """
+  Maps keys of `map` using function `f`.
+
+  ## Example
+
+      iex> #{inspect(__MODULE__)}.map_keys(%{1 => :a, 2 => :b}, & &1+1)
+      %{2 => :a, 3 => :b}
+
+  """
+  @spec map_keys(%{k1 => v}, (k1 -> k2)) :: %{k2 => v} when k1: any, k2: any, v: any
+  def map_keys(map, f) do
+    map |> Enum.into(Map.new(), fn {key, value} -> {f.(key), value} end)
+  end
+
+  @doc """
+  Maps values of `map` using function `f`.
+
+  ## Example
+
+      iex> #{inspect(__MODULE__)}.map_values(%{a: 1, b: 2}, & &1+1)
+      %{a: 2, b: 3}
+
+  """
+  @spec map_values(%{k => v1}, (v1 -> v2)) :: %{k => v2} when k: any, v1: any, v2: any
+  def map_values(enum, f) do
+    enum |> Enum.into(Map.new(), fn {key, value} -> {key, f.(value)} end)
+  end
+end
---
+++ test/bunch/map_test.exs
@@ -0,0 +1,7 @@
+defmodule Bunch.MapTest do
+  use ExUnit.Case, async: true
+
+  @module Bunch.Map
+
+  doctest @module
+end
