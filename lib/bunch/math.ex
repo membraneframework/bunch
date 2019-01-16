7d2c459a6671a8b472137293d0d73f1871ff2c71
Mateusz Front <mateusz.front@swmansion.com>
2019-01-16 16:12:21 +0100

    refactor of Math.div_err

--- lib/bunch/math.ex
+++ lib/bunch/math.ex
@@ -4,34 +4,44 @@ defmodule Bunch.Math do
   """
 
   @doc """
-  Works like `div/2`, but compensates division error when it reaches 1.
-
-  When there is some period (e.g. of time) calculated as `divident / divisor`
-  that has to be integer, but integer division introduces an error, this error
-  grows each time the period passes. This function accumulates the error
-  and periodically adds compensating component to the result.
-
-  Returned value is a tuple consisting of result and updated error. This error
-  needs to be passed to subsequent call of the function. Initial error should be
-  set to 0. `error` represents error relatively to the divisor (real error can
-  be expressed as `error / divisor`), thus error is valid only for the same
-  divisor.
+  Applies `div/2` and `rem/2` to arguments and returns results as a tuple.
 
   ## Example
 
-      iex> 1..10 |> Enum.map_reduce(0, fn _, err -> #{inspect(__MODULE__)}.div_err(9, 4, err) end)
+      iex> #{inspect(__MODULE__)}.div_rem(10, 4)
+      {div(10, 4), rem(10, 4)}
+
+  """
+  @spec div_rem(divident :: non_neg_integer, divisor :: pos_integer) ::
+          {div :: non_neg_integer, rem :: non_neg_integer}
+  def div_rem(dividend, divisor) do
+    {div(dividend, divisor), rem(dividend, divisor)}
+  end
+
+  @doc """
+  Works like `div_rem/2` but allows to accumulate remainder.
+
+  Useful when an accumulation of division error is not acceptable, for example
+  when you need to produce chunks of data every second but need to make sure there
+  are 9 chunks per 4 seconds on average. You can calculate `div_rem(9, 4)`,
+  keep the remainder, pass it to subsequent calls and every third result will be
+  bigger than others.
+
+  ## Example
+
+      iex> 1..10 |> Enum.map_reduce(0, fn _, err ->
+      ...>  #{inspect(__MODULE__)}.div_rem(9, 4, err)
+      ...>  end)
       {[2, 2, 2, 3, 2, 2, 2, 3, 2, 2], 2}
 
   """
-  @spec div_err(divident :: non_neg_integer, divisor :: pos_integer, error :: non_neg_integer) ::
-          {result :: non_neg_integer, error :: non_neg_integer}
-  def div_err(dividend, divisor, error \\ 0) when error < divisor do
-    error = error + rem(dividend, divisor)
-
-    if error < divisor do
-      {div(dividend, divisor), error}
-    else
-      {div(dividend, divisor) + 1, error - divisor}
-    end
+  @spec div_rem(
+          divident :: non_neg_integer,
+          divisor :: pos_integer,
+          accumulated_remainder :: non_neg_integer
+        ) ::
+          {div :: non_neg_integer, rem :: non_neg_integer}
+  def div_rem(dividend, divisor, accumulated_remainder) when accumulated_remainder < divisor do
+    div_rem(accumulated_remainder + dividend, divisor)
   end
 end
