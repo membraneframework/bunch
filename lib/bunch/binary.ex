c2a8c665693eaa7a3d0dbd68a3c69b30860b5c84
Mateusz Front <mateusz.front@swmansion.com>
2020-01-13 16:19:39 +0100

    add Bunch.Binary.(try_)map_while/2

--- lib/bunch/binary.ex
+++ lib/bunch/binary.ex
@@ -6,6 +6,74 @@ defmodule Bunch.Binary do
   use Bunch
 
   @doc """
+  Returns a tuple `{list, rest}` where each element of the list is the result of
+  invoking `f` on a part of binary, and rest is the unprocessed leftover.
+
+  Function `f` is always given entire non-consumed part binary and is supposed to
+  return a subsequent output element and the remaining part of the binary to be
+  processed by further calls to the function. Once the remaining data becomes
+  ineligible for processing, the function should return `:halt`.
+
+  ## Examples
+
+      iex> f = fn
+      iex>   <<size, content::binary-size(size), rest::binary>> -> {content, rest}
+      iex>   _binary -> :halt
+      iex> end
+      iex> #{inspect(__MODULE__)}.map_while(<<2, "ab", 3, "cde", 4, "fghi">>, f)
+      {~w(ab cde fghi), <<>>}
+      iex> #{inspect(__MODULE__)}.map_while(<<2, "ab", 3, "cde", 4, "fg">>, f)
+      {~w(ab cde), <<4, "fg">>}
+
+  """
+  @spec map_while(binary, (binary -> {a, binary} | :halt)) :: {[a], binary} when a: any
+  def map_while(binary, f) do
+    do_map_while(binary, f, [])
+  end
+
+  defp do_map_while(binary, f, acc) do
+    case f.(binary) do
+      {value, rest} -> do_map_while(rest, f, [value | acc])
+      :halt -> {Enum.reverse(acc), binary}
+    end
+  end
+
+  @doc """
+  The version of `map_while/2` that accepts `:ok` and `:error` return tuples.
+
+  Mapping is continued as long as `:ok` tuples are returned, upon `:error` it breaks
+  and the error is returned.
+
+  ## Examples
+
+      iex> f = fn
+      iex>   <<a, b, rest::binary>> ->
+      iex>     sum = a + b
+      iex>     if rem(sum, 2) == 1, do: {:ok, {sum, rest}}, else: {:error, :even_sum}
+      iex>   _binary -> {:ok, :halt}
+      iex> end
+      iex> #{inspect(__MODULE__)}.try_map_while(<<1,2,3,4,5>>, f)
+      {:ok, {[3, 7], <<5>>}}
+      iex> #{inspect(__MODULE__)}.try_map_while(<<2,4,6,8>>, f)
+      {:error, :even_sum}
+
+  """
+  @spec try_map_while(binary, (binary -> {:ok, {a, binary} | :halt} | {:error, reason})) ::
+          {:ok, {[a], binary}} | {:error, reason}
+        when a: any, reason: any
+  def try_map_while(binary, f) do
+    do_try_map_while(binary, f, [])
+  end
+
+  defp do_try_map_while(binary, f, acc) do
+    case f.(binary) do
+      {:ok, {value, rest}} -> do_try_map_while(rest, f, [value | acc])
+      {:ok, :halt} -> {:ok, {Enum.reverse(acc), binary}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @doc """
   Chunks given binary into parts of given size.
 
   Remaining part is cut off.
