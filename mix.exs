6cdec291556354019adfc1232274ade4079619b1
Mateusz Front <mateusz.front@swmansion.com>
2020-05-07 15:20:26 +0200

    update dialyxir

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Bunch.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
 end
