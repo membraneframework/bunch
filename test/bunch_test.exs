bb8e2bcaee36a1fc299d10020bb4e1c94a818256
Mateusz Front <mateusz.front@swmansion.com>
2022-09-07 11:29:14 +0200

    warn on unused withl clause (#37)
    * warn on unused withl clause
    
    * Update lib/bunch.ex
    
    Co-authored-by: Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
    
    * Update lib/bunch.ex
    
    * improve withl doctests
    
    * make withl warning doctest a regular test
    
    Co-authored-by: Bartosz Błaszków <bartosz.blaszkow@swmansion.com>

--- test/bunch_test.exs
+++ test/bunch_test.exs
@@ -1,7 +1,31 @@
 defmodule BunchTest do
+  use Bunch
   use ExUnit.Case, async: true
+
+  import ExUnit.CaptureIO
 
   @module Bunch
 
   doctest @module
+
+  test "withl warns on redundant else labels" do
+    warns =
+      capture_io(:stderr, fn ->
+        quote do
+          withl a: _a <- 123,
+                b: _a = 123 do
+            :ok
+          else
+            a: _a -> :error
+            b: _b -> :error
+            c: _c -> :error
+          end
+        end
+        |> Code.compile_quoted()
+      end)
+
+    refute String.contains?(warns, "withl's else clause labelled :a will never match")
+    assert String.contains?(warns, "withl's else clause labelled :b will never match")
+    assert String.contains?(warns, "withl's else clause labelled :c will never match")
+  end
 end
