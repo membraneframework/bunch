2aaa16cd3c32750005b82041f754d10dd8c310bc
Mateusz Front <mateusz.front@swmansion.com>
2022-11-16 15:33:30 +0100

    add require? option to Bunch.Config.parse (#40)
    * add require? option to Bunch.Config
    
    * support function returning fields specs, deprecate require_if option in Bunch.Config.parse/2
    
    * fix spec of Bunch.Config.parse
    
    * fix Bunch.Config.parse
    
    * add more tests for Config.parse

--- lib/bunch/config.ex
+++ lib/bunch/config.ex
@@ -14,8 +14,13 @@ defmodule Bunch.Config do
     * validate - function determining if field's value is correct
     * in - enumerable containing all valid values
     * default - value returned if a field is not found in `config`
-    * require_if - function determining if a field is required basing on previous
-      fields' values
+    * require? - determines whether a field is required, defaults to `false` when `default` is set
+      and `true` when `default` is not set
+    * require_if - deprecated, pass function returning constraints instead
+
+  Instead of a list of constraints, a function accepting fields parsed so far and returning
+  the constraints can be passed. If the function returns `nil`, field is considered non existent,
+  as if it wasn't passed at all.
 
   ## Examples
 
@@ -34,40 +39,44 @@ defmodule Bunch.Config do
       ...> [a: 1, b: 2],
       ...> a: [validate: & &1 > 0],
       ...> b: [in: -2..2],
-      ...> c: [require_if: & &1.a == &1.b]
+      ...> c: [require?: false]
       ...> )
       {:ok, %{a: 1, b: 2}}
       iex> #{inspect(__MODULE__)}.parse(
       ...> [a: 1, b: 1],
       ...> a: [validate: & &1 > 0],
       ...> b: [in: -2..2],
-      ...> c: [require_if: & &1.a == &1.b]
+      ...> c: &(if &1.a == &1.b, do: [in: 0..1])
       ...> )
       {:error, {:config_field, {:key_not_found, :c}}}
 
   """
   @spec parse(
           config :: Keyword.t(v),
-          fields_specs ::
-            Keyword.t(
-              validate:
-                (v | any -> Type.try_t() | boolean)
-                | (v | any, config_map -> Type.try_t() | boolean),
-              in: list(v),
-              default: v,
-              require_if: (config_map -> boolean)
-            )
-        ) :: Type.try_t(config_map)
-        when config_map: %{atom => v}, v: any
+          [field | {field, field_specs | (parsed_config -> field_specs)}]
+        ) :: Type.try_t(parsed_config)
+        when parsed_config: %{atom => v},
+             field: atom,
+             v: any,
+             field_specs:
+               [
+                 validate:
+                   (v | any -> Type.try_t() | boolean)
+                   | (v | any, parsed_config -> Type.try_t() | boolean),
+                 in: Enumerable.t(),
+                 default: v,
+                 require?: boolean,
+                 require_if: (parsed_config -> boolean)
+               ]
+               | nil
   def parse(config, fields_specs) do
-    fields_specs = fields_specs |> Bunch.KVList.map_values(&Map.new/1)
-
     withl kw: true <- config |> Keyword.keyword?(),
           dup: [] <- config |> Keyword.keys() |> Bunch.Enum.duplicates(),
           do: config = config |> Map.new(),
           fields:
-            {:ok, {config, remaining}} when remaining == %{} <- parse_fields(config, fields_specs) do
-      {:ok, config}
+            {:ok, remaining_config, parsed_config} when remaining_config == %{} <-
+              parse_fields(config, fields_specs, %{}) do
+      {:ok, parsed_config}
     else
       kw: false ->
         {:error, {:config_not_keyword, config}}
@@ -75,26 +84,52 @@ defmodule Bunch.Config do
       dup: duplicates ->
         {:error, {:config_duplicates, duplicates}}
 
-      fields: {{:error, reason}, _config} ->
+      fields: {:error, reason} ->
         {:error, {:config_field, reason}}
 
-      fields: {:ok, {_config, remaining}} ->
-        {:error, {:config_invalid_keys, remaining |> Map.keys()}}
+      fields: {:ok, remaining_config, _parsed_config} ->
+        {:error, {:config_invalid_keys, Map.keys(remaining_config)}}
     end
   end
 
-  defp parse_fields(config, fields_specs) do
-    fields_specs
-    |> Bunch.Enum.try_reduce({%{}, config}, fn {key, spec}, {acc, remaining} ->
-      case parse_field(key, spec, remaining |> Map.fetch(key), acc) do
-        {:ok, {key, value}} -> {:ok, {acc |> Map.put(key, value), remaining |> Map.delete(key)}}
-        :ok -> {:ok, {acc, remaining}}
-        {:error, reason} -> {{:error, reason}, config}
+  defp parse_fields(config, [], parsed_config) do
+    {:ok, config, parsed_config}
+  end
+
+  defp parse_fields(config, [field_spec | fields_specs], parsed_config) do
+    {key, spec} =
+      case field_spec do
+        key when is_atom(key) -> {key, []}
+        {key, spec} when is_list(spec) -> {key, spec}
+        {key, spec} when is_function(spec, 1) -> {key, spec.(parsed_config)}
       end
-    end)
+
+    if spec == nil do
+      parse_fields(config, fields_specs, parsed_config)
+    else
+      case parse_field(key, Map.new(spec), Map.fetch(config, key), parsed_config) do
+        {:ok, {key, value}} ->
+          parse_fields(Map.delete(config, key), fields_specs, Map.put(parsed_config, key, value))
+
+        {:ok, :ignore} ->
+          parse_fields(Map.delete(config, key), fields_specs, parsed_config)
+
+        {:error, :field_not_accepted} ->
+          parse_fields(config, fields_specs, parsed_config)
+
+        {:error, reason} ->
+          {:error, reason}
+      end
+    end
   end
 
   defp parse_field(key, %{require_if: require_if} = spec, value, config) do
+    require Logger
+
+    Logger.warn(
+      "Passing :require_if option to Bunch.Config.parse/2 is deprecated, pass function returning constraints instead"
+    )
+
     spec = spec |> Map.delete(:require_if)
 
     cond do
@@ -105,12 +140,16 @@ defmodule Bunch.Config do
         parse_field(key, spec, value, config)
 
       true ->
-        :ok
+        {:error, :field_not_accepted}
     end
   end
 
   defp parse_field(key, %{default: default}, :error, _config) do
     {:ok, {key, default}}
+  end
+
+  defp parse_field(_key, %{require?: false}, :error, _config) do
+    {:ok, :ignore}
   end
 
   defp parse_field(key, _spec, :error, _config) do
--- test/bunch/config_test.exs
+++ test/bunch/config_test.exs
@@ -4,4 +4,22 @@ defmodule Bunch.ConfigTest do
   @module Bunch.Config
 
   doctest @module
+
+  test "config parsing" do
+    assert {:error, {:config_field, {:key_not_found, :c}}} ==
+             @module.parse(
+               [a: 1, b: 1],
+               a: [validate: &(&1 > 0)],
+               b: [in: -2..2],
+               c: []
+             )
+
+    assert {:error, {:config_invalid_keys, [:c, :d]}} ==
+             @module.parse(
+               [a: 1, b: 1, c: 2, d: 3],
+               a: [validate: &(&1 > 0)],
+               b: [in: -2..2],
+               c: &if(&1.a != &1.b, do: [])
+             )
+  end
 end
