65aafd828a0ef2a3dabaa763d1f0def8a549ff37
varsill <lukasz.kita0@gmail.com>
2022-11-24 10:56:17 +0100

    Add then_if example with piping

--- lib/bunch.ex
+++ lib/bunch.ex
@@ -460,6 +460,9 @@ defmodule Bunch do
     1
     iex> then_if(1, true, & &1 + 1)
     2
+    iex> arg = 1
+    iex> arg |> then_if(not is_list(arg), fn arg -> [arg] end) |> Enum.map(&(&1*2))
+    [2]
   """
   @spec then_if(x, condition :: boolean(), f :: (x -> y)) :: y when x: any(), y: any()
   def then_if(x, condition, f) do
